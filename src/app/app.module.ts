81c546ce6a595b83aa6e9d78a93ff86f14d1d2e4
dostonbekkhamidov@gmail.com <doston1994iyul23>
2021-10-15 17:17:56 +0500

    News Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainScreenComponent } from './main-screen/main-screen.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { NewsComponent } from './news/news.component';
+import {ButtonModule} from "primeng/button";
+import {CardModule} from "primeng/card";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainScreenComponent
+    MainScreenComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
